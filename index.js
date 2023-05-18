fa1d02171a98f435410c060d2e3949ff249c3c96
boredrum <vvmaiboroda@gmail.com>
2023-05-18 10:29:47 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,40 @@
+                                        // EXCERCISE 1
+
+// let userNames = ['Mike', 'Bob', 'Nikola'];
+// let users = {};
+
+// [users.mike, users.bob, users.nikola] = userNames;
+
+// console.log(users);
+
+
+                                        // EXCERCISE 2
+
+// let salaries = {
+//     "Mike": 1500,
+//     "Bob": 1800,
+//     "Nikola": 1700
+// };
+
+// let maxSalary = function(salaries){
+//     let maxWage = {};
+//     for (const[key, value] of Object.entries(salaries)){
+//         if (!maxWage.salary || maxWage.salary < value){
+//             [maxWage.name, maxWage.salary] = [key, value]
+//         }
+//     }
+//     return maxWage.name
+// }
+
+// console.log(maxSalary(salaries));
+
+
+                                        // EXCERCISE 3
+
+// let users = {mike: 'Mike', bob: 'Bob', nikola: 'Nikola'};
+
+// let {mike: userMike, bob: userBob, nikola: userNikola} = users;
+
+// console.log(userMike);
+// console.log(userBob);
+// console.log(userNikola);
